d39f1aef17449be072f36483c63f9105805f30f3
levkovich-polina <levkovich.polina@gmail.com>
2023-10-19 21:38:24 +0300

    Completed 1 task

--- Array_Task1/Array_Task1/Program.cs
+++ Array_Task1/Array_Task1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,52 +14,41 @@ namespace Array_Task1
             Random _rnd = new Random();
 
             int N = 20;
-            int[] arrayN = new int[N];
-
-            for (int i = 0; i < N; i++)
-            {
-                arrayN[i] = _rnd.Next(100);
-            }
-
+            int[] arrayN = InitializeArray(N);
             Console.WriteLine("Исходный массив:");
-            for (int i = 0; i < arrayN.Length; i++)
-            {
-                Console.Write($" {arrayN[i]} ");
-            }
+            PrintArray(arrayN, arrayN.Length, 0);
             Console.WriteLine();
 
             int M = _rnd.Next(1, 11);
-            int[] arrayM = new int[M];
-
-            for (int i = 0; i < M; i++)
-            {
-                arrayM[i] = _rnd.Next(100);
-            }
+            int[] arrayM = InitializeArray(M);
             Console.WriteLine("Добавляемые элементы:");
-            for (int i = 0; i < arrayM.Length; i++)
-            {
-                Console.Write($" {arrayM[i]} ");
-            }
+            PrintArray(arrayM, arrayM.Length, 0);
             Console.WriteLine();
 
-            int K = _rnd.Next(0, arrayN.Length);    
+            int K = _rnd.Next(0, arrayN.Length);
             Console.WriteLine($"После {K} элемента будут добавляться новые числа.");
-
-            for (int j = 0; j < K; j++)
-            {
-                Console.Write($" {arrayN[j]} ");
-            }
-
-            for (int j = 0; j < M; j++)
-            {
-                Console.Write($" {arrayM[j]} ");
-            }
-
-            for (int j = K; j < N; j++)
-            {
-                Console.Write($" {arrayN[j]} ");
-            }
+            PrintArray(arrayN,K, 0);
+            PrintArray(arrayM, arrayM.Length, 0);
+            PrintArray(arrayN, arrayN.Length, K);
             Console.ReadLine();
+        }
+        static int[] InitializeArray(int n)
+        {
+            Random random = new Random();
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1,101);
+            }
+
+            return array;
+        }
+        static void PrintArray(int[] array, int l, int s)
+        {
+            for (int i = s; i < l; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
         }
     }
 }
